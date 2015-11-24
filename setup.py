ebb1b1bb080b0c960bcf37cf81d09185cec4fc6d
Kasper Jacobsen <k@mackwerk.dk>
2015-11-24 11:25:30 +0100

    Bump pep257 version

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 
 setup(
     name='__dummy_package',
-    version='0.0.0',
-    install_requires=['pep257==0.3.2'],
+    version='0.0.1',
+    install_requires=['pep257==0.7.0'],
 )
